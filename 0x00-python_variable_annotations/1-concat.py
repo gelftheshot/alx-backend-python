196e9bdebdeb151124ece1b49e1d65c9823ed490
Leon Gebre <gelf.learing@gmail.com>
2024-04-22 15:02:46 +0300

    take two string and returng the string of concatination

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+""" type annotated function that take two string and return
+    concatnated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ return the concatnated string of two string
+    """
+    return str1 + str2
