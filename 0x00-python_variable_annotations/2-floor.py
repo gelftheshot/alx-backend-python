30f7f25a1981b0612d71618a649625111cf35eec
Leon Gebre <gelf.learing@gmail.com>
2024-04-22 15:06:13 +0300

    floar of a function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+    type-annotated function floor which takes a float n as
+    argument and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+        return the floor of the float
+    """
+    return int(n)
