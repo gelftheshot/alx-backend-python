ec0846b4ca94dc7af597de222787123f87dc0f96
Leon Gebre <gelf.learing@gmail.com>
2024-04-22 20:36:41 +0300

    time measurment

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+''' 
+    maeasur the running time for a_wait function
+'''
+
+from time import time
+from asyncio import run
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    ''' Return execution time for wait_n given `n` and `max_delay`. '''
+    time_0 = time()
+    run(wait_n(n, max_delay))
+    time_1 = time()
+    elapsed_time = time_1 - time_0
+    return elapsed_time / n
