3411a146edf6d10a94989e66916a964f8c11c5df
Leon Gebre <gelf.learing@gmail.com>
2024-04-22 15:23:51 +0300

    chaging built in list to typing list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,8 +3,9 @@
 """type-annotated function sum_list which takes a list input_list of floats
   as argument and returns their sum as a float."""
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
         return the sum of the list of float
     """
