a7312c96cd3580b99ef7237cdf3f1ce8c324c757
Leon Gebre <gelf.learing@gmail.com>
2024-04-23 20:56:34 +0300

    different approch for 2-measur_runtim

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
+'''
+    Desription: Import async_comprehension from the previous file and write a
+'''
 
-""" measure the time for excution of the functions """
-from time import time
 from asyncio import gather
-async_comprehension = __import__("1-async_comprehension").async_comprehension
+from time import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """ measure the tim taken for comprtion"""
-    time_0 = time()
-    await gather(
-            async_comprehension(), async_comprehension(),
-            async_comprehension(), async_comprehension())
-    time_1 = time()
-    return time_1 - time_0
+    ''' Measure the runtime of async_comprehension executed 4 times in
+        parallel. '''
+    first_time = time()
+    await gather(async_comprehension(), async_comprehension(),
+                 async_comprehension(), async_comprehension())
+    next_time = time()
+
+    return next_time - first_time
