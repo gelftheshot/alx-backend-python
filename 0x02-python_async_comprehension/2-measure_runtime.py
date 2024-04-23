811163c1dffd5a8e1736d3f3969b3d8a76409579
Leon Gebre <gelf.learing@gmail.com>
2024-04-23 20:43:05 +0300

    changin the way things are

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
 """ measure the time for excution of the functions """
-import time
-import asyncio
+from time import time
+from asyncio import gather
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
 async def measure_runtime() -> float:
     """ measure the tim taken for comprtion"""
-    time_0 = time.time()
-    await asyncio.gather(
+    time_0 = time()
+    await gather(
             async_comprehension(), async_comprehension(),
-            async_comprehension(), async_comprehension()
-            )
-    time_1 = time.time()
-    return(time_1 - time_0)
+            async_comprehension(), async_comprehension())
+    time_1 = time()
+    return time_1 - time_0
