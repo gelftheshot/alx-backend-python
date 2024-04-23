64fd4cda0b74e77561efa3bf6ea26324876fc04d
Leon Gebre <gelf.learing@gmail.com>
2024-04-23 20:38:36 +0300

    fixing the return type

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -6,7 +6,7 @@ import asyncio
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
-async def measure_runtime() -> int:
+async def measure_runtime() -> float:
     """ measure the tim taken for comprtion"""
     time_0 = time.time()
     await asyncio.gather(
