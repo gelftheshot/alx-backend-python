2c8712df9f1868324acd35f5ee287e50d00c8a64
Leon Gebre <gelf.learing@gmail.com>
2024-05-13 11:27:58 +0300

    testing if a method is called only once

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,6 +2,7 @@
 """ a test class for utils in test_utils file """
 access_nested_map = __import__('utils').access_nested_map
 get_json = __import__('utils').get_json
+memoize = __import__('utils').memoize
 
 import unittest
 from parameterized import parameterized
@@ -40,3 +41,22 @@ class TestGetJson(unittest.TestCase):
 
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
+
+class TestMemoize(unittest.TestCase):
+    """ testing the memoizetion function is working or not """
+    
+    def test_memoize(self):
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+            
+        with patch.object(TestClass, 'a_method', return_value = 42) as mock_method:
+            test_obj = TestClass()
+            self.assertEqual(test_obj.a_property, 42)
+            self.assertEqual(test_obj.a_property, 42)
+            mock_method.assert_called_once()
