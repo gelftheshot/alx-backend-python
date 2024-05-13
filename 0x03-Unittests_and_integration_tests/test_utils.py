572d9eb11d1feb481cdca265ec4a0cf917b20094
Leon Gebre <gelf.learing@gmail.com>
2024-05-13 11:44:03 +0300

    fixing pycodesytle

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,39 +1,47 @@
 #!/usr/bin/env python3
 """ a test class for utils in test_utils file """
-access_nested_map = __import__('utils').access_nested_map
-get_json = __import__('utils').get_json
-memoize = __import__('utils').memoize
 
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
 
+access_nested_map = __import__('utils').access_nested_map
+get_json = __import__('utils').get_json
+memoize = __import__('utils').memoize
+
+
 class TestAccessNestedMap(unittest.TestCase):
     """ a test class for ascessnestmap fucnon """
 
-    @parameterized.expand([({"a": 1}, ("a",),1),
-                        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-                        ({"a": {"b": 2}}, ("a", "b"), 2)])
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
     def test_access_nested_map(self, nested_map, path, e_result):
         """ the main method to test """
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, e_result)
-    
-    @parameterized.expand([({}, ("a",), KeyError),
-                        ({"a", 1}, ("a", "b"), KeyError),])
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a", 1}, ("a", "b"), KeyError)
+    ])
     def test_access_nested_map_exception(self, nested_map, path, e_error):
         """ the excepting checker """
         with self.assertRaises(e_error):
             access_nested_map(nested_map, path)
-    
+
+
 class TestGetJson(unittest.TestCase):
     """ testing get json from url """
 
     @parameterized.expand([
-       ("http://example.com", {"payload": True}),
-       ("http://holberton.io", {"payload": False})])
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
     @patch('requests.get')
-    def test_get_json(self, test_url,test_payload, mock_get):
+    def test_get_json(self, test_url, test_payload, mock_get):
         """ mocking get function from utils """
         mock_response = Mock()
         mock_response.json.return_value = test_payload
@@ -42,9 +50,10 @@ class TestGetJson(unittest.TestCase):
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
 
+
 class TestMemoize(unittest.TestCase):
     """ testing the memoizetion function is working or not """
-    
+
     def test_memoize(self):
         """ TESTING if memoize momoize or not """
         class TestClass:
@@ -56,10 +65,10 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_met:
             test_obj = TestClass()
             real_return = test_obj.a_property
             real_return = test_obj.a_property
 
             self.assertEqual(real_return, 42)
-            mock_method.assert_called_once()
+            mock_met.assert_called_once()
